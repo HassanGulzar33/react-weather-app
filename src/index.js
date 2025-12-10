b7de7dfb6e6fd09e4e6062d0640c608b0e06cbaf
Hassan Gulzar <165555955+HassanGulzar33@users.noreply.github.com>
2025-12-10 17:53:11 +0500

    Add comment for performance measurement guidance
    Added a comment regarding performance measurement.

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const config = {
 
 
 serviceWorkerRegistration.register(config);
-
+//Adding a new comment in Index.js file as a Devops 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
